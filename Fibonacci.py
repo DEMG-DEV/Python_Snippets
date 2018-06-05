8d7e76fcd25137f85a2ed91af79ab225df9575db
David Mendez Guardado <demg@outlook.com>
2018-06-04 22:59:10 -0500

    First snippets of code with Python

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Este metodo sirve para hacer la secuencia Fibonacci
+def fibonacci(ant,sig,max,tot):
+    if ant == sig:
+        print(ant)
+        print(sig)    
+    print(ant+sig)
+    tot += 1
+    if tot < max:
+        fibonacci(sig,(ant+sig),max,tot)
+
+max = int(input("Numero de veces que se genere la secuencia Fibonacci:"))
+numero = 1
+fibonacci(numero,numero,max,0)
---
+++ factorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#Calculo del Factorial de un numero
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+#Se pide al usuario que introduzca el numero a calcular
+number = int(input("Enter a number:"))
+print(fact(number))
